8f89263b8069605b50596284f654dc1a9bfeb11e
Jan Rusnacko <rusnackoj@gmail.com>
2014-05-13 13:29:52 +0200

    Adding Dir::compare method

--- lib/ruby_util/dir.rb
+++ lib/ruby_util/dir.rb
@@ -6,8 +6,14 @@
 
 class Dir
   def eql?(dir)
+    self.class.compare(self, dir)
+  end
+
+  def self.compare(dir1, dir2)
+    dir1 = dir1.to_path if dir1.is_a? Dir
+    dir2 = dir2.to_path if dir2.is_a? Dir
     # Todo: add to requirements
-    %x{diff #{self.to_path} #{dir.to_path}}
+    %x{diff #{dir1} #{dir2}}
     return $? == 0
   end
 end
--- spec/ruby_util/dir_spec.rb
+++ spec/ruby_util/dir_spec.rb
@@ -8,11 +8,28 @@ require 'spec_helper'
 require 'ruby_util/dir'
 
 describe Dir do
-  it 'should correctly compare equal directories' do
-    Dir.new(FIXTURES_DIR).should be_eql(Dir.new(FIXTURES_DIR))
+  context 'eql?' do
+    it 'should return true when comparing to equal directory' do
+      Dir.new(FIXTURES_DIR).should be_eql(Dir.new(FIXTURES_DIR))
+    end
+
+    it 'should return false when comparing to different directory' do
+      Dir.new(FIXTURES_DIR).should_not be_eql(Dir.new(FIXTURES_DIR + "/../"))
+    end
   end
 
-  it 'should correctly compare different directories' do
-    Dir.new(FIXTURES_DIR).should_not be_eql(Dir.new(FIXTURES_DIR + "/../"))
+  context 'compare' do
+    it 'should return true when comparing the same directories' do
+      Dir.compare(FIXTURES_DIR, FIXTURES_DIR).should be_true
+      Dir.compare(FIXTURES_DIR, Dir.new(FIXTURES_DIR)).should be_true
+      Dir.compare(Dir.new(FIXTURES_DIR), FIXTURES_DIR).should be_true
+    end
+
+    it 'should return false when comparing different directories' do
+      Dir.compare(FIXTURES_DIR, FIXTURES_DIR + "/../").should be_false
+      Dir.compare(Dir.new(FIXTURES_DIR), FIXTURES_DIR + "/../").should be_false
+      Dir.compare(FIXTURES_DIR, Dir.new(FIXTURES_DIR + "/../")).should be_false
+    end
+
   end
 end
